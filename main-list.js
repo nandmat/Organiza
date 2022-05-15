31db9f984010df4da92c861ff0c5bcf0407cdfb1
Nanderson Matheus Reis <87582433+nandmat@users.noreply.github.com>
2022-05-15 19:36:16 -0300

    Add files via upload

---
+++ main-list.js
@@ -0,0 +1,13 @@
+function adicionar() {
+
+    let text = document.getElementById('texto').value;
+
+    let list = document.getElementById('lista').innerHTML;
+
+
+    list += '<li>' +text+ '</li>';
+
+    document.getElementById('lista').innerHTML = list;
+
+    document.getElementById('texto').value = null;
+}
